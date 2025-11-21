14262778558928b1c4dc0aefd503974941b1c4a9
bikash451 <bkashchaudhary02@gmail.com>
2025-11-21 17:12:28 +0530

    Update Projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,6 +13,35 @@ export const Projects = () => {
                 <h2 className="text-3xl font-bold text-center mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">Featured Projects</h2>
 
                 <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
+
+  <div className="rounded-xl p-6 border border-white/10 hover:-translate-y-1 transition-all hover:border-blue-500/30 hover:shadow-[0_5px_20px_rgba(59,130,246,0.3)]">
+    <h3 className="text-xl font-bold mb-3">Bypass Authentication Demo</h3>
+    <p className="text-gray-400 mb-4">
+        This project demonstrates a brute-force attempt to bypass login on a local website using a simple web application. 
+    </p>
+    <div className="flex flex-wrap gap-2 mb-4">
+      {["React.js", "Node.js", "express.js", "Postgresql","Tailwind CSS"].map((tech, index) => (
+        <span 
+          key={index}
+          className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:shadow-[0_5px_10px_rgba(59,130,246,0.4)]"
+        >
+          {tech}
+        </span>
+      ))}
+    </div>
+    <div className="flex justify-between items-center">
+    <a
+       target="blank" href="https://github.com/bikash451/Bypass-Authentication"
+        className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+    > <FaGithub className="ml-5"/>
+
+        View Project →                 
+    </a>
+</div>
+
+
+  </div>
+
   <div className="rounded-xl p-6 border border-white/10 hover:-translate-y-1 transition-all hover:border-blue-500/30 hover:shadow-[0_5px_20px_rgba(59,130,246,0.3)]">
     <h3 className="text-xl font-bold mb-3">Market-Place</h3>
     <p className="text-gray-400 mb-4">
